340045bc9f8d620d89c51646de24d5c8d392dd99
AinurWin <ainurbogdanov69@gmail.com>
2025-07-06 08:53:50 +0400

    made app save data in localStorage instead of backend

--- src/app/common-ui/todo-list/todo-list.component.ts
+++ src/app/common-ui/todo-list/todo-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { Todo } from '../../interfaces/todo';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../../services/auth/auth.service';
+import { StorageService } from '../../services/storage/storage.service';
 
 @Component({
   selector: 'app-todo-list',
@@ -13,57 +15,89 @@ export class TodoListComponent {
   @Output() emitter = new EventEmitter<Todo[]>();
 
   private http = inject(HttpClient);
+  private authService = inject(AuthService);
+  private storageService = inject(StorageService);
+
+  private lastId = 1;
 
   ngOnInit() {
     this.loadTodos();
   }
 
   loadTodos() {
-    this.http.get<Todo[]>(this.getApiUrl()).subscribe({
-      next: (data) => {
-        this.todoList = data;
-      },
-      error: (err) => console.error('Ошибка загрузки', err),
-    });
+    if (this.authService.isAuthenticated()) {
+      this.http.get<Todo[]>(this.getApiUrl()).subscribe({
+        next: (data) => {
+          this.todoList = data;
+        },
+        error: (err) => console.error('error', err),
+      });
+    } else {
+      this.todoList = this.storageService.loadFromStorage();
+    }
   }
 
   addTodo(title: string) {
-    this.http.post<Todo>(this.getApiUrl(), { title: title }).subscribe({
-      next: () => {
-        this.loadTodos();
-      },
-      error: (err) => console.error('Ошибка добавления:', err),
-    });
+    if (this.authService.isAuthenticated()) {
+      this.http.post<Todo>(this.getApiUrl(), { title: title }).subscribe({
+        next: () => {
+          this.loadTodos();
+        },
+        error: (err) => console.error('error', err),
+      });
+    } else {
+      const newTodo: Todo = {
+        id: this.getNextId(),
+        title,
+        completed: false,
+      };
+
+      this.todoList.push(newTodo);
+
+      this.storageService.saveToStorage(this.todoList);
+    }
   }
 
   deleteTodo(id: number) {
-    this.http.delete(`${this.getApiUrl()}/${id}`).subscribe({
-      next: () => {
-        // Удаляем задачу из локального массива (без перезагрузки)
-        this.todoList = this.todoList.filter((todo) => todo.id !== id);
-      },
-      error: (err) => console.error('Ошибка удаления:', err),
-    });
+    if (this.authService.isAuthenticated()) {
+      this.http.delete(`${this.getApiUrl()}/${id}`).subscribe({
+        next: () => {
+          this.todoList = this.todoList.filter((todo) => todo.id !== id);
+        },
+        error: (err) => console.error('error', err),
+      });
+    } else {
+      this.todoList = this.todoList.filter((todo) => {
+        return todo.id !== id;
+      });
+      this.storageService.saveToStorage(this.todoList);
+    }
   }
 
   changeStatusTodo(id: number) {
     const activeTodo = this.todoList.filter((todo) => todo.id === id);
-    this.http
-      .patch(`${this.getApiUrl()}/${id}`, { completed: !activeTodo[0].completed })
-      .subscribe({
-        next: () => {
-          this.loadTodos();
-        },
-        error: (err) => console.log('failed patching todo', err),
-      });
-  }
 
-  updateList() {
-    const data = this.todoList;
-    this.emitter.emit(data);
+    if (this.authService.isAuthenticated()) {
+      this.http
+        .patch(`${this.getApiUrl()}/${id}`, { completed: !activeTodo[0].completed })
+        .subscribe({
+          next: () => {
+            this.loadTodos();
+          },
+          error: (err) => console.log('failed patching todo', err),
+        });
+    } else {
+      const index = this.todoList.findIndex((todo) => todo.id === id);
+      if (index !== -1) {
+        this.todoList[index].completed = !this.todoList[index].completed;
+      }
+    }
   }
 
   private getApiUrl(): string {
     return 'http://localhost:3000/todos';
+  }
+  private getNextId(): number {
+    return ++this.lastId;
   }
 }
