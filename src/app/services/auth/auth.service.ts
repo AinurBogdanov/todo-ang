839ddabe6902a0ad3d3d9407331d691f75b393fe
AinurWin <ainurbogdanov69@gmail.com>
2025-07-06 04:43:44 +0400

    auth service readonly, inject and prettier

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,50 +1,50 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { tap } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 import { environment } from '../../../environments';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private TOKEN_KEY = 'access_token';
-  private readonly API_URL = environment.API_URL; 
-
-  constructor(
-    private http: HttpClient,
-    private cookieService: CookieService
-  ) {}
+  private readonly API_URL = environment.API_URL;
+  private readonly http = inject(HttpClient);
+  private readonly cookieService = inject(CookieService);
 
   register(username: string, password: string) {
-    return this.http.post(`${this.API_URL}/register`, { username, password})
+    return this.http
+      .post(`${this.API_URL}/register`, { username, password })
       .pipe(
         tap((response: any) => {
-            if(response.access_token) {
-              this.saveToken(response.access_token);
-            }
-          }  
-        )
-      )
-  };
+          if (response.access_token) {
+            this.saveToken(response.access_token);
+          }
+        })
+      );
+  }
 
   isAuthenticated(): boolean {
     return !!this.getToken(); // Проверяем наличие токена
-  } 
+  }
 
   logout(): void {
     // Удаляем токен при выходе
   }
 
   logIn(username: string, password: string) {
-    return this.http.post(`${this.API_URL}/login`, { username, password })
-      .pipe(
-        tap((response: any) => {
-          if(response.access_token) {
-            this.saveToken(response.access_token)
-          }
-        })
-      )
+    return this.http.post(`${this.API_URL}/login`, { username, password }).pipe(
+      tap((response: any) => {
+        if (response.access_token) {
+          this.saveToken(response.access_token);
+        }
+      })
+    );
+  }
+
+  getToken(): string | null {
+    return this.cookieService.get(this.TOKEN_KEY);
   }
 
   private saveToken(token: string): void {
@@ -52,11 +52,7 @@ export class AuthService {
       expires: 1,
       path: '/',
       secure: true,
-      sameSite: 'Strict'
-    })
-  }
-
-  getToken(): string | null{
-    return this.cookieService.get(this.TOKEN_KEY)
+      sameSite: 'Strict',
+    });
   }
 }
